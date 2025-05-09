d7712d15bb6f2303b68339962e5ffc4227722002
BGDataGuru <bg.ai.hub@gmail.com>
2025-05-09 20:10:09 +0530

    commit

---
+++ 05-modules/package/__init__.py
@@ -0,0 +1,3 @@
+''' __init__.py is a special Python file that indicates that the directory should be treated as a package.
+    It can be empty, or it can contain code to initialize the package.
+'''
---
+++ 05-modules/package/subpackages/maths.py
@@ -0,0 +1,7 @@
+def addition(a, b):
+    print("Performing addition using function in package.subpackages.maths")
+    return a + b
+
+def subtraction(a, b):
+    print("Performing subtraction using function in package.subpackages.maths")
+    return a - b
